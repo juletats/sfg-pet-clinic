8f99680c018d3f1621f8856e90e9e3cb153f3bba
juletats <juleta-tsaturyan@mail.ru>
2022-09-26 23:18:37 +0300

    Updated Map Impl and set auto ID.  Closed #23

--- pet-clinic-data/src/main/java/com/juletats/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/juletats/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,27 +1,47 @@
 package com.juletats.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.juletats.sfgpetclinic.model.BaseEntity;
 
-public abstract  class AbstractMapService <T,ID>{
-protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
 
-Set<T> findAll(){
-    return  new HashSet<>(map.values());
-}
-    T findById(ID id){
-    return map.get(id);
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
+    private Long getNextId(){
+        Long nextId=null;
+        try {
+           nextId= Collections.max(map.keySet()) + 1;
+        }
+        catch (NoSuchElementException e){
+            nextId=1L;
+        }
+        return nextId;
     }
-    T save(ID id, T object){
-    map.put(id, object);
-    return object;
+    Set<T> findAll() {
+        return new HashSet<>(map.values());
     }
-    void deleteById( ID id){
-    map.remove(id);
+
+    T findById(ID id) {
+        return map.get(id);
     }
-    void delete(T object){
-    map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+
+    T save( T object) {
+        if(object!=null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+        map.put(object.getId(),object);
+        }
+        else{
+            throw new RuntimeException("Object cannot be null!");
+        }
+        return object;
+    }
+
+    void deleteById(ID id) {
+        map.remove(id);
+    }
+
+    void delete(T object) {
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 }
